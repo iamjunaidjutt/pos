8fdeb32f75bbd04eadc8a136ebe18e59d2aefc2d
iamjunaidjutt <info.iamjunaidjutt@gmail.com>
2023-11-21 17:11:04 +0500

    in progress to change the Product class

---
+++ src/main/java/com/scd/Models/Product.java
@@ -0,0 +1,14 @@
+package com.scd;
+
+import java.util.Locale.Category;
+import java.util.Objects;
+
+public class Product {
+    private String code;
+    private String name;
+    private String description;
+    private double price;
+    private int stockQuantity;
+    private List<Category> categories;
+
+}
